1a8ee1bc3e5b8494edf9d451598fcc135f878b71
imkyle94 <andy3638@naver.com>
2022-06-16 23:03:13 +0900

    숫자 맞히기 난수 생성 코드

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,17 @@
 use std::io;
- 
+use std::cmp::Ordering;
+// Ordering 열거자 : Less, Greater, Equal
+use rand::Rng;
+// Rng 트레이트(trait) : 객체지향 언어에서의 인터페이스 + 추상 클래스 짬뽕.
+
 fn main() {
     println!("숫자를 맞혀봅시다!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    // 크레이트를 사용하는 방법은 각 크레이트의 문서를 참고하는 것이 일반적이다.
+    // cargo doc --open 명령 실행 시 의존 패키지 문서를 보여준다.
+
+    println!("사용자가 맞혀야 할 숫자: {}", secret_number);
 
     println!("정답이라고 생각하는 숫자를 입력하세요.");
 
@@ -18,5 +28,16 @@ fn main() {
     // Result 타입 : 열거자(enums, or enumerations) : 미리 정의된 범위의 값을 갖는 타입
     // Result 열거자는 Ok와 Err(에러)를 가지고 있다. 즉, 에러 처리에 사용된다.
 
+    // 러스트는 변수 가리기(shadowing)를 사용한다. 타입 변환시 보통 사용한다.
+    // parse : 구문 분석
+    let guess: u32 = guess.trim().parse()
+        .expect("입력한 값이 올바른 숫자가 아닙니다.");
+
     println!("입력한 값: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("입력한 숫자가 작습니다!"),
+        Ordering::Greater => println!("입력한 숫자가 큽니다!"),
+        Ordering::Equal => println!("정답!"),
+    }
 }
