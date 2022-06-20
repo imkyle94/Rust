5481640d970157d3645e411b450570a4b852c7aa
ParkJungho <andy3638@naver.com>
2022-06-20 17:16:10 +0900

    열거자

---
+++ enums/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    enum IpAddrKind {
+        v4,
+        v6,
+    }
+    
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::v4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::v6,
+        address: String::from("::1"),
+    };
+}
